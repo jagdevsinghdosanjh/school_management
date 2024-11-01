bb17081d27a771c23bcee15a220d5d6adc116972
Jagdev Singh <jagdevsinghdosanjh@gmail.com>
2024-11-01 08:48:15 +0530

    Ok

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 include './index_layout/header.php';
 include('./index_layout/footer.php'); 
 
-echo '<h1>Welcome to School Management System</h1>'
+echo '<h5>Welcome to School Management System</h5>'
 
 ?>
